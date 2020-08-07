9b426ad63f17b649fb0b4dcac16e1497e0d80fd3
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-07 14:38:19 +0530

    Update Delete nodes.cpp

--- Delete nodes.cpp
+++ Delete nodes.cpp
@@ -134,3 +134,10 @@ int main()
  }
  return 0;
 }
+
+/*
+1) Node to be deleted is leaf: Simply remove from the tree.
+2) Node to be deleted has only one child: Copy the child to the node and delete the child.
+3) Node to be deleted has two children: Find inorder successor of the node. Copy contents of the inorder successor to the node and delete the inorder successor. 
+Note that inorder predecessor can also be used.
+*/
