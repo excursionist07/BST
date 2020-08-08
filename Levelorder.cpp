559005241e02915457f18c6e2c0994de4b3de767
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-08 13:11:08 +0530

    Update Levelorder.cpp

--- Levelorder.cpp
+++ Levelorder.cpp
@@ -1,76 +1,39 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-struct node
-{
-  ll data;
-  node *left;
-  node *right;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root)
+    {
+     vector<vector<int>>ans;
+     if(!root)
+         return ans;
+     queue<TreeNode*>q;
+     q.push(root);
+     while(!q.empty())
+     {
+      vector<int>vv;
+      int zz=q.size();
+      for(int i=0;i<zz;i++)
+      {
+      TreeNode* cur=q.front();
+      q.pop();
+      vv.push_back(cur->val);
+      if(cur->left)
+          q.push(cur->left);
+      if(cur->right)
+          q.push(cur->right);
+      }
+      ans.push_back(vv);     
+     }
+     return ans;
+    }
 };
-
-node *GetNewNode(ll x)
-{
- node *temp=new node();
- temp->data=x;
- temp->left=temp->right=NULL;
- return temp;
-}
-
-node *Insert(node *root,ll x)
-{
- if(root==NULL)
-    root=GetNewNode(x);
- else if(x<=root->data)
-    root->left=Insert(root->left,x);
- else
-    root->right=Insert(root->right,x);
- return root;
-}
-
-void LevelOrder(node *root)
-{
- queue<node*>q;
- q.push(root);
- while(!q.empty())
- {
-  node *cur=q.front();
-  cout<<cur->data<<" ";
-  q.pop();
-  if(cur->left)q.push(cur->left);
-  if(cur->right)q.push(cur->right);
- }
-}
-int main()
-{
- hs;
- ll t;
- cin>>t;
- f(t)
- {
-  node *root=NULL;
-  ll n;
-  cin>>n;
-  f(n)
-  {
-    ll x;
-    cin>>x;
-    root=Insert(root,x);
-  }
-  LevelOrder(root);
- }
- return 0;
-}
