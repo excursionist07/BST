1948c0d523fbe9ace499b813e0dc13ba21702ae4
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-10 03:21:10 +0530

    Update Delete nodes greater than k.cpp

--- Delete nodes greater than k.cpp
+++ Delete nodes greater than k.cpp
@@ -1,25 +1,3 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-#include<vector>
-vector<int>vec;
-
-Node* temp2;
-
 Node *FindMin(Node *root)
 {
   if(root==NULL)
@@ -74,40 +52,4 @@ Node *Delete(Node *root,int y)
   }
 
   return root;
-}
-void in(Node *root,int x)
-{
-
-  if(root==NULL)
-    return;
-  in(root->left,x);
-  if(root->data>=x)
-     vec.push_back(root->data);
-  in(root->right,x);
- // return temp2;
-}
-/*void in2(Node *root)
-{
-  if(root==NULL)
-    return;
-  in2(root->left);
-  cout<<root->data<<" ";
-  in2(root->right);
-}*/
-Node* deleteNode(Node* root, int key)
-{
-  // Node* temp1=root;
-  // Node* temp2=root;
-    vec.clear();
-   in(root,key);
-   Node *temp2=root;
-   for(int i=0;i<vec.size();i++)
-   {
-     temp2=Delete(temp2,vec[i]);
-   }
-   return temp2;
-
-
-
-
 }
