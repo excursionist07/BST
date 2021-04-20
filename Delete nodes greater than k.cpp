4d131cf649e0a0e46c5921fe77a8740a015cb6cb
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-04-21 00:31:09 +0530

    Update Delete nodes greater than k.cpp

--- Delete nodes greater than k.cpp
+++ Delete nodes greater than k.cpp
@@ -1,55 +1,59 @@
-Node *FindMin(Node *root)
-{
-  if(root==NULL)
-    return NULL;
-  else if(root->left==NULL)
-    return root;
-  else
-     FindMin(root->left);
-}
 
-Node *Delete(Node *root,int y)
-{
-  if(root==NULL)
-    return NULL;
-  else if(y<root->data)
-     root->left=Delete(root->left,y);
-  else if(y>root->data)
-     root->right=Delete(root->right,y);
-  else
-  {
-      //No child
-  if(root->left==NULL && root->right==NULL)
-  {
-     delete root;
-     root=NULL;
+class Solution {
+public:
+    TreeNode* FindMin(TreeNode* root)
+    {
+     if(!root)
+         return NULL;
+     else if(!root->left)
+         return root;
+     else
+        return FindMin(root->left);
+    
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) 
+    {
+     if(!root)
+         return NULL;
+     else if(root->val>key)
+         root->left=deleteNode(root->left,key);
+     else if(root->val<key)
+         root->right=deleteNode(root->right,key);
+     else
+     {
+      // no child
+      if(!root->left && !root->right)
+      {
+       delete root;
+       root=NULL;
+       return root;
+      }
+      // one child
+      else if(!root->left)
+      {
+       TreeNode* temp=root;
+       root=root->right;
+       delete temp;
+       return root;
+      }
+      else if(!root->right)
+      {
+       TreeNode* temp=root;
+       root=root->left;
+       delete temp;
+       return root;
+      }
+      // two child
+      else
+      {
+       TreeNode* temp=FindMin(root->right);
+       root->val=temp->val;
+       root->right=deleteNode(root->right,temp->val);
+       return root;
+      }
+     }
      return root;
-  }
-  // one child
-  else if(root->left==NULL)
-  {
-     Node *temp=root;
-     root=root->right;
-     delete temp;
-     return root;
-  }
-  else if(root->right==NULL)
-  {
-    Node *temp=root;
-    root=root->left;
-    delete temp;
-    return root;
-
-  }
-  // Two child
-  else
-  {
-    Node *temp=FindMin(root->right);
-    root->data=temp->data;
-    root->right=Delete(root->right,temp->data);
-    return root;
-  }
-  }
-
-  return root;
-}
+         
+       
+    }
+};
